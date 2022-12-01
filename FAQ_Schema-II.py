50f0ad1476a2649bdc9693a47e889821a095a4a3
MeetChauhan03 <99242018+MeetChauhan03@users.noreply.github.com>
2022-12-01 12:21:41 +0530

    UpdateFAQ-Schema

--- FAQ_Schema-II.py
+++ FAQ_Schema-II.py
@@ -8,26 +8,31 @@ driver=webdriver.Chrome()
 faqs=[]
 # quest={}
 # ans={}
-
-driver.get('https://larknorthgate.com/faqs/')
-
+web=input("Enter the url: ")
+# https://larknorthgate.com/faqs/
+try:
+    driver.get(web)
+except:
+    print("Please Make sure you are connected to internet")
+    
 content=driver.page_source
 soup=BeautifulSoup(content,'html.parser')
 
-for faq in soup.find_all('div',class_='collapse-items'):
-    quest=faq.find('h2', class_='title')
-    ans=faq.find('p',class_='content')
-    faq_dic={
-        'question': quest.text,
-        'answer':ans.text
-    }
-    faqs.append(faq_dic)
-    # quest.append(quest.text)
-    # print(quest.text)
-    # ans.append(ans.text)
-    # print(ans.text)
+def schema():
+    for faq in soup.find_all('div',class_='collapse-items'):
+        quest=faq.find('h2', class_='title')
+        ans=faq.find('p',class_='content')
+        faq_dic={
+            'question': quest.text,
+            'answer':ans.text
+        }
+        faqs.append(faq_dic)
+        # quest.append(quest.text)
+        # print(quest.text)
+        # ans.append(ans.text)
+        # print(ans.text)
 
-    with open ("schema.json","w") as f :
-        f.write(json.dumps(faqs))
+        with open ("schema.json","w") as f :
+            f.write(json.dumps(faqs))
 
-print(json.dumps(faqs))
+    print(json.dumps(faqs))
