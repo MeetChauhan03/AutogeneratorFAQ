134cefc569cf74cf30e1c7d2e5b266c50277390d
MeetChauhan03 <99242018+MeetChauhan03@users.noreply.github.com>
2022-12-05 12:08:38 +0530

    Update FAQ_Schema.py

--- FAQ_Schema.py
+++ FAQ_Schema.py
@@ -1,33 +1,53 @@
-import json
 from selenium import webdriver
-# from BeautifulSoup import BeautifulSoup
-from bs4 import  BeautifulSoup
-import pandas as pd
+from bs4 import BeautifulSoup
+import json
+# import os
 
-data=webdriver.Chrome() #"/usr/lib/chromium-browser/chromedriver"
 
-# sudo find / -type f -name chromedriver  ---- this will find the path of your chrome driver
-questions={}#dic to store questions
-answer={}#dic to store answers
-# web=input("enter website FAQ page url: ")
-data.get('https://larknorthgate.com/faqs/')#website url
-# data.get(web)
+web = input("Enter Your Url: ")
+div=input("select the type of tag parent: \n Eg:- div : ")
+paren_class=input("enter class in which Q & A there: ")
+div2=input('Enter question tag: ')
+quest_class=input("enter question class: ")
+div3=input('Enter answer tag: ')
+answer_class=input("enter answer class:")
 
-content = data.page_source
-soup = BeautifulSoup(content)
+driver = webdriver.Chrome()
+faqs=[]
 
-for a in soup.findAll('div',class_='collapse-items'):
-    # questions=a.find('div',attrs={'class':'title'})
-    questons=a.find('h2', class_='title')
-    answer=a.find('p',class_='content')
-# questions=data.find_element_by_xpath('//span[@class="auto-link"][1]')
-    # questions.append(questions.txt)
-    print(questions.text)
-    # answer.append(answer.txt)
-    print(answer.text)
 
-# df= pd.DataFrame({'Questions':questions,'Answer':answer})
-# df.to_csv('products.csv', index=False, encoding='utf-8')
-# df.to_json('FAQ.json')
+def schema():
+    for faq in soup.find_all(div,class_=paren_class):
+        quest=faq.find(div2,class_=quest_class)
+        ans=faq.find(div3,class_=answer_class)
+        faq_dic={
+        #    <script type="application/ld+json"> 
+        #    {
+            # "@context": "https://schema.org",
+            # "@type": "FAQPage",
+            # "mainEntity": {
+                "@type": "Question",
+                "name": quest.text,
+                "acceptedAnswer": 
+                {"@type": "Answer",
+                 "text": ans.text
+                }
+                # }
+                # }
+                # </script>
+                }
+        faqs.append(faq_dic)
+        print(quest.text)
+        with open("schema.json","w") as f:
+            f.write(json.dumps(faqs))
+print(json.dumps(faqs))
 
-print(json.dump())
+if web == False:
+    print("Pllease try again later...\n value can't be null")
+    quit()
+else:
+    driver.get(web)
+    content=driver.page_source
+    soup=BeautifulSoup(content,'html.parser')
+
+schema()
